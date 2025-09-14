e327367271d40f7272baa4efcc3d5c89f5fad3c4
Remo <2227393@slu.edu.ph>
2025-09-14 21:03:26 +0800

    Added a simple schedule of removing oldChirps and added a solf delete (deleted at) column in the chirps

---
+++ app/Console/Commands/removeOldChirps.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Chirp;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class removeOldChirps extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:remove-old-chirps';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command will remove chirps older than a day in the database to keep the database clean.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        try {
+            $oneDayAgo = Carbon::now()->subDay();
+            $chirps = Chirp::where('created_at', '<', $oneDayAgo)->delete();
+            $count = Chirp::where('created_at', '<', $oneDayAgo)->get()->count();
+
+            Log::info("Found $count chirps older than a day to remove.");
+            Log::info('Removing chirps older than: ' . $oneDayAgo->toDateTimeString());
+            Log::info("Removed $count old chirps from the database.");
+        } catch (\Throwable $th) {
+            Log::error('Error removing old chirps: ' . $th->getMessage());
+            $this->error('An error occurred while removing old chirps. Check the logs for details.');
+        }
+    }
+}
--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Chirp extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'chrips';
 
     protected $fillable = [
